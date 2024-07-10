695d1be7e5097ed7c2426cdc2253b7001764e585
dkool <dkool@dkools-MacBook-Pro.local>
2024-07-10 14:11:22 +0200

    feat: change random provider version constraints

--- main.tf
+++ main.tf
@@ -2,24 +2,22 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.3.2"
+      version = "~> 3.6"
     }
   }
-  required_version = "~> 1.0" 
+  required_version = "~> 1.0"
 }
 
 resource "random_string" "main" {
   length  = 60
   special = false
   upper   = false
-  numeric  = var.unique-include-numbers
 }
 
 resource "random_string" "first_letter" {
   length  = 1
   special = false
   upper   = false
-  numeric  = false
 }
 
 
