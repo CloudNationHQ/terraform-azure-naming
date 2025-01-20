93a53b6ca0dba06d3ffea70f42eed8cbcdf4c39a
eddy-vera <122470243+eddy-vera@users.noreply.github.com>
2025-01-20 15:47:40 +0100

    feat: add testing.md + terraform.tf to separate file

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-  }
-  required_version = "~> 1.0"
-}
-
 resource "random_string" "main" {
   length  = 60
   special = false
